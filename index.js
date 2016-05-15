9e02e15a02e4a3c32c573425c92477577712bada
abeyahmad <abey@elucent.io>
2016-05-14 23:19:33 -0400

    Updated index.js
    Certain file names had the wrong case. This would cause failure on UNIX system

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-module.exports.gliderecord = require('./gliderecord.js');
-module.exports.glideaggregate = require('./glideaggregate.js')
+module.exports.gliderecord = require('./GlideRecord.js');
+module.exports.glideaggregate = require('./GlideAggregate.js')
 module.exports.importset = require('./importset.js')
 module.exports.attachment = require('./attachment.js')
 module.exports.userrole = require('./userrole.js')
